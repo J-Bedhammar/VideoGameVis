5550d328875cca6a8c9a231cdce9f29d04165a8d
Sparvolle <Oliverj96@hotmail.com>
2019-02-21 10:02:51 +0100

    infoViz update, bar chart

--- js/infoViz.js
+++ js/infoViz.js
@@ -1,9 +1,9 @@
 
 function infoViz(data){
 
-
 	gameSales = d3.csv.parse(data, function(d){
-		return {Name: d.Name,
+		return {
+		Name: d.Name,
 		Platform: d.Platform,
 		Year_of_Release: d.Year_of_Release,
 		Genre: d.Genre,
@@ -21,5 +21,33 @@ function infoViz(data){
 		Rating: d.Rating
 		};
 	);
+	
+	/*
+	//Bar Chart
+	
+	//TEMP VALUES!
+	var x = d3.scale.ordinal().rangeRoundBands([0, 100], .1);	
+	var y = d3.scale.linear().range([100, 0]);
+	
+	//creates axes for the bar chart 
+	var xAxisBar = d3.svg.axis().scale(x).orient("bottom");
+	var yAxisBar = d3.svg.axis().scale(y).orient("left");
+	
+	//create tip here
+	
+	//CHANGE "body"
+	var svg = d3.select("body").appen("svg").append("g");
+	
+	// svg.call(tip);
+	
+	svg.append("g").attr("class", "x axis").call(xAxisBar);
+	svg.append("g").attr("class", "y axis").call(yAxisBar);
+	
+	svg.selectAll(".bar").data(gameSales).enter().append("rect").attr("class","bar")
+		.attr("x", function(d) {return x(d.Name);}).attr("y", function(d) { return y(d.Global_Sales);})
+	//.on('mouseover', tip.show).on('mouseout',tip.hide);
+		
+	
+	*/
 	
 }
