80c4bad2894e78bfb0a509b3ef686337a60d9573
Jennifer Bedhammar <jennifer95@hotmail.com>
2019-02-22 09:00:36 +0100

    Added annualSales

--- js/infoViz.js
+++ js/infoViz.js
@@ -1,12 +1,13 @@
 
 function infoViz(data){
 	
+	//Print outs
 	console.log("infoViz()");
-	
 	console.log("First game: " + data[0].Name);
 	
+	var tempPublisher = data[0].Publisher;
 	
-	
+	annualSales = new annualSales(data, tempPublisher);
 	
 	/*
 	//Bar Chart
