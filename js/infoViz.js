9e9afa448a6807be6cc3c87dd0ba5d2c83a58737
Jennifer Bedhammar <jennifer95@hotmail.com>
2019-02-22 08:37:39 +0100

    Fixed color, added js scripts and read in csv

--- js/infoViz.js
+++ js/infoViz.js
@@ -1,26 +1,12 @@
 
 function infoViz(data){
-
-	gameSales = d3.csv.parse(data, function(d){
-		return {
-		Name: d.Name,
-		Platform: d.Platform,
-		Year_of_Release: d.Year_of_Release,
-		Genre: d.Genre,
-		Publisher: d.Publisher,
-		NA_Sales: +d.NA_Sales,
-		EU_Sales: +d.EU_Sales,
-		JP_Sales: +d.JP_Sales,
-		Other_Sales: +d.Other_Sales,
-		Global_Sales: +d.Global_Sales,
-		Critic_Score: +d.Critic_Score,
-		Critic_Count: +d.Critic_Count,
-		User_Score: +d.User_Score,
-		User_Count: +d.User_Count,
-		Developer: d.Developer,
-		Rating: d.Rating
-		};
-	);
+	
+	console.log("infoViz()");
+	
+	console.log("First game: " + data[0].Name);
+	
+	
+	
 	
 	/*
 	//Bar Chart
--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-queue()
-  .defer(d3.csv,'data/Video_Games_Sales_as_at_22_Dec_2016.csv')
-  .await(readData);
-
+d3.queue()
+	.defer(d3.csv, 'data/Video_Games_Sales_as_at_22_Dec_2016.csv')
+	.await(main)
+	
 var infoViz;
 
-function readData(error, data){
-  if (error) throw error;
-
-  //Send in dataset
-  infoViz = new infoViz(data);
+function main(error, data){
+	if(error) throw(error);
+	
+	console.log("main()");
+	
+	//Send in dataset
+	infoViz = new infoViz(data);
 
 }
