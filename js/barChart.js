00ebbdd2377ff171d63181c0157f9c3a914c59df
Sparvolle <Oliverj96@hotmail.com>
2019-02-22 16:17:26 +0100

    BarChart added

--- js/barChart.js
+++ js/barChart.js
@@ -1,34 +1,75 @@
 
 function barChart(data){
 	
+	var top5 = [];
+	top5.push(data[100]);
+	top5.push(data[92]);
+	top5.push(data[20]);
+	top5.push(data[19]);
+	top5.push(data[4]);
+	
+	top5.forEach(function(d) {
+		d.Global_Sales = + d.Global_Sales;
+	});
+	
+	top5.sort(function(a, b) { return a.Global_Sales - b.Global_Sales; });
+	
+	
 	//Bar Chart
-	var container = $("bar-chart");
-	var containerWidth = $("bar-chart");
+	var container = $("#bar-chart");
+	var containerWidth = $("#bar-chart").width()*0.7;
+	var containerHeight = $("#bar-chart").height()*0.7;
 	
+	marginTop = 20;
+	marginLeft = 150;
 	
-	//TEMP VALUES!
-	var x = d3.scale.ordinal().rangeRoundBands([0, 100], .1);	
-	var y = d3.scale.linear().range([100, 0]);
+	var x = d3.scaleLinear().range([0, containerWidth]);
+	var y = d3.scaleBand().range([containerHeight, 0]).padding(0.3);
+	var names = d3.scaleBand().range([containerHeight, 0]).padding(0.3);
 	
-	//creates axes for the bar chart 
-	var xAxisBar = d3.svg.axis().scale(x).orient("bottom");
-	var yAxisBar = d3.svg.axis().scale(y).orient("left");
-	
-	//create tip here
-	
-	//CHANGE "body"
-	var svg = d3.select("body").appen("svg").append("g");
-	
-	// svg.call(tip);
-	
-	svg.append("g").attr("class", "x axis").call(xAxisBar);
-	svg.append("g").attr("class", "y axis").call(yAxisBar);
-	
-	svg.selectAll(".bar").data(gameSales).enter().append("rect").attr("class","bar")
-		.attr("x", function(d) {return x(d.Name);}).attr("y", function(d) { return y(d.Global_Sales);})
-	//.on('mouseover', tip.show).on('mouseout',tip.hide);
+	var svg = d3.select("#bar-chart").append("svg")
+			.attr("width", containerWidth*1.5)
+			.attr("height", containerHeight*1.1)
+			.append("g")
+			.attr("transform", "translate(" + marginLeft + ',' + marginTop + ")");
 		
 	
-	*/
+	
+	y.domain(top5.map(function(d) { return d.Name;}));
+	x.domain([0, d3.max(top5, function(d) { return d.Global_Sales;})]);
+	names.domain(top5.map(function(d) { 
+		if( d.Name.length > 26)
+			return d.Name.substring(0,23) + "...";
+		else
+			return d.Name;
+	}));
+	
+	
+	var tooltip = d3.select("#bar-chart").append("div").attr("class","toolTip");
+	
+	svg.selectAll(".bar")
+		.data(top5)
+		.enter().append("rect")
+		.attr("class","bar")
+		.attr("width", function(d) { return x(d.Global_Sales); })
+		.attr("y", function(d) { return y(d.Name); })
+		.attr("height", y.bandwidth() )
+		.on("mouseover", function(d) { 
+			tooltip.style("display", "inline-block")
+			.style("left", d3.event.pageX - 50 + "px")
+			.style("top", d3.event.pageY - 70 + "px")
+			.html("<strong>Global Sales: </strong>" + "<strong>" + (d.Global_Sales));
+		})
+		.on("mouseout", function(d){ tooltip.style("display", "none");});
+	
+	
+	//creates axes for the bar chart 	
+	svg.append("g").call(d3.axisTop(x));
+	svg.append("g").call(d3.axisLeft(names)).on("mouseover", function(d) { 
+			tooltip.style("display", "inline-block")
+			.style("left", d3.event.pageX - 50 + "px")
+			.style("top", d3.event.pageY - 70 + "px")
+			.html("<strong>HELA NAMNET: </strong>");
+		});
 	
 }
--- js/infoViz.js
+++ js/infoViz.js
@@ -5,36 +5,12 @@ function infoViz(data){
 	console.log("infoViz()");
 	console.log("First game: " + data[0].Name);
 	
-	var tempPublisher = data[0].Publisher;
+	barChart(data)
 	
-	annualSales = new annualSales(data, tempPublisher);
+	//var tempPublisher = data[0].Publisher;
 	
-	/*
-	//Bar Chart
+	//annualSales = new annualSales(data, tempPublisher);
 	
-	//TEMP VALUES!
-	var x = d3.scale.ordinal().rangeRoundBands([0, 100], .1);	
-	var y = d3.scale.linear().range([100, 0]);
 	
-	//creates axes for the bar chart 
-	var xAxisBar = d3.svg.axis().scale(x).orient("bottom");
-	var yAxisBar = d3.svg.axis().scale(y).orient("left");
-	
-	//create tip here
-	
-	//CHANGE "body"
-	var svg = d3.select("body").appen("svg").append("g");
-	
-	// svg.call(tip);
-	
-	svg.append("g").attr("class", "x axis").call(xAxisBar);
-	svg.append("g").attr("class", "y axis").call(yAxisBar);
-	
-	svg.selectAll(".bar").data(gameSales).enter().append("rect").attr("class","bar")
-		.attr("x", function(d) {return x(d.Name);}).attr("y", function(d) { return y(d.Global_Sales);})
-	//.on('mouseover', tip.show).on('mouseout',tip.hide);
-		
-	
-	*/
 	
 }
